7822a0604585307f74c066ed708ce6353bf10662
bruno <brunohjs@gmail.com>
2022-12-10 22:39:30 -0300

    New version released v0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="rasa-model-report",
-    version="0.0.2",
+    version="0.0.3",
     author="Bruno Justo",
     author_email="brunohjs@gmail.com",
     license="Apache 2.0",
