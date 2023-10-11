55ba9d10ad4919487391274425ca9ec9524cec36
bruno <brunohjs@gmail.com>
2023-10-11 16:35:30 -0300

    86 - Beta release feature

--- scripts/release.py
+++ scripts/release.py
@@ -11,6 +11,19 @@ from typing import Optional
 
 logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO)
 MAIN_BRANCH = "main"
+DEVELOP_BRANCH = "develop"
+
+
+def check_params() -> None:
+    """
+    Check if parameters are valid.
+    """
+    if len(sys.argv) > 1:
+        if sys.argv[1] not in ["major", "minor", "patch"]:
+            raise error_message("Invalid version type. Use 'major', 'minor' or 'patch'.")
+    if len(sys.argv) > 2:
+        if sys.argv[2] not in ["beta"]:
+            raise error_message("Invalid version type. Use 'beta'.")
 
 
 def close_milestone(version: str) -> None:
@@ -27,9 +40,7 @@ def close_milestone(version: str) -> None:
         ], shell=True)
         logging.info("")
     else:
-        error_message = f"Milestone {version} not found."
-        logging.error(error_message)
-        raise Exception(error_message)
+        raise error_message(f"Milestone {version} not found.")
 
 
 def get_changelog(version: str) -> Optional[str]:
@@ -48,9 +59,7 @@ def get_changelog(version: str) -> Optional[str]:
             logging.info(f"Changelog notes found for version {version}.")
             return re.sub(regex, "", text).strip()
     else:
-        error_message = f"No notes found for version {version}. Update CHANGELOG.md file."
-        logging.error(error_message)
-        raise Exception(error_message)
+        raise error_message(f"No notes found for version {version}. Update CHANGELOG.md file.")
 
 
 def create_release(version: str, notes: str) -> None:
@@ -84,6 +93,55 @@ def update_version_setup_file(new_version: str) -> None:
     file.close()
 
 
+def error_message(message: str) -> None:
+    """
+    Print error message and raise Exception.
+    """
+    logging.error(message)
+    return Exception(message)
+
+
+def get_latest_version() -> str:
+    """
+    Get latest version from Github tags.
+
+    :return: Latest version.
+    """
+    subprocess.run(["git", "fetch", "--all"], shell=False)
+    versions = subprocess.check_output(["git", "tag"]).decode('ascii').strip().split('\n')
+    version = versions[-1] if versions and versions[0] else '0.0.0'
+    return version
+
+
+def get_new_version_type() -> str:
+    """
+    Get new version type from command line.
+
+    :return: New version type.
+    """
+    if len(sys.argv) > 2 and sys.argv[1] in ["major", "minor", "patch"]:
+        if sys.argv[1] == "major":
+            return "major"
+        elif sys.argv[1] == "minor":
+            return "minor"
+        return "patch"
+    raise error_message("Invalid version type. Use 'major', 'minor' or 'patch'.")
+
+
+def check_beta_version() -> bool:
+    """
+    Check if new version is a beta version.
+
+    :return: True if new version is a beta version.
+    """
+    if len(sys.argv) > 2:
+        return sys.argv[2] == "beta"
+    elif sys.argv:
+        return False
+    else:
+        raise error_message("Invalid version type. Use 'beta'.")
+
+
 def get_new_version() -> str:
     """
     Get current version from Github tags and return new version.
@@ -91,31 +149,37 @@ def get_new_version() -> str:
     :return: New version.
     """
     logging.info("Getting last version")
-    subprocess.run(["git", "fetch", "--all"], shell=False)
-    versions = subprocess.check_output(["git", "tag"]).decode('ascii').strip().split('\n')
-    version = versions[-1] if versions and versions[0] else '0.0.0'
+    version = get_latest_version()
     logging.info(f"Last version: v{version}")
     version = version.split('.')
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "major":
+    version_type = get_new_version_type()
+    if "b" in version[2]:
+        if check_beta_version():
+            splitted_patch = version[2].split("b")
+            version[2] = splitted_patch[0]
+            version.append("b" + str(int(splitted_patch[1]) + 1))
+        else:
+            splitted_patch = version[2].split("b")
+            version[2] = splitted_patch[0]
+    else:
+        if check_beta_version():
+            version.append("b1")
+        if version_type == "major":
             version[0] = str(int(version[0]) + 1)
             version[1] = "0"
             version[2] = "0"
-        elif sys.argv[1] == "minor":
+        elif version_type == "minor":
             version[1] = str(int(version[1]) + 1)
             version[2] = "0"
-        elif sys.argv[1] == "patch":
+        elif version_type == "patch":
             version[2] = str(int(version[2]) + 1)
-        logging.info(f"New {sys.argv[1]} version")
-    else:
-        version[2] = str(int(version[2]) + 1)
-        logging.info("New patch version")
-    version = ".".join(version)
+    logging.info(f"New {version_type} version")
+    version = ".".join(version[:3]) + "".join(version[3:])
     logging.info(f"New version: v{version}")
     return version
 
 
-def create_tag(version: str) -> None:
+def create_tag(version: str, beta_version: bool = False) -> None:
     """
     Create a commit with version update and release new tag on Github.
 
@@ -123,28 +187,48 @@ def create_tag(version: str) -> None:
     """
     branch_name = subprocess.check_output(["git", "branch", "--show-current"]).decode('ascii')
     logging.info(f"Current branch: {branch_name}")
-    if branch_name != MAIN_BRANCH:
-        logging.info(f"Switching to branch {MAIN_BRANCH}")
-        subprocess.run([f"git checkout '{MAIN_BRANCH}'"], shell=True)
-    logging.info(f"Updating branch {MAIN_BRANCH}")
-    subprocess.run(["git pull"], shell=True)
-    logging.info("Updating setup.py file")
-    update_version_setup_file(version)
-    logging.info("Committing updates")
-    subprocess.run(["git add setup.py"], shell=True)
-    subprocess.run([f"git commit -n -m \"New version released v{version}\""], shell=True)
-    subprocess.run(["git push"], shell=True)
-    logging.info("Committing tag")
-    subprocess.run([f"git tag {version}"], shell=True)
-    subprocess.run(["git", "push", "origin", f"{version}"])
-    if os.path.isdir("dist"):
-        shutil.rmtree("dist")
+    if beta_version:
+        logging.info("Updating setup.py file")
+        update_version_setup_file(version)
+        logging.info("Committing updates")
+        subprocess.run(["git add setup.py"], shell=True)
+        subprocess.run([f"git commit -n -m \"New beta version v{version}\""], shell=True)
+        subprocess.run(["git push"], shell=True)
+        logging.info("Committing tag")
+        subprocess.run([f"git tag {version}"], shell=True)
+        subprocess.run(["git", "push", "origin", f"{version}"])
+        if os.path.isdir("dist"):
+            shutil.rmtree("dist")
+    else:
+        if branch_name != MAIN_BRANCH:
+            logging.info(f"Switching to branch {MAIN_BRANCH}")
+            subprocess.run([f"git checkout '{MAIN_BRANCH}'"], shell=True)
+        logging.info(f"Updating branch {MAIN_BRANCH}")
+        subprocess.run(["git pull"], shell=True)
+        logging.info("Updating setup.py file")
+        update_version_setup_file(version)
+        logging.info("Committing updates")
+        subprocess.run(["git add setup.py"], shell=True)
+        subprocess.run([f"git commit -n -m \"New released version v{version}\""], shell=True)
+        subprocess.run(["git push"], shell=True)
+        logging.info("Committing tag")
+        subprocess.run([f"git tag {version}"], shell=True)
+        subprocess.run(["git", "push", "origin", f"{version}"])
+        if os.path.isdir("dist"):
+            shutil.rmtree("dist")
+        logging.info(f"Updating branch {DEVELOP_BRANCH}")
+        subprocess.run([f"git checkout '{DEVELOP_BRANCH}'"], shell=True)
+        subprocess.run([f"git merge {MAIN_BRANCH}"], shell=True)
+        subprocess.run(["git push"], shell=True)
     logging.info(f"Finished process. New version v{version}")
 
 
 if __name__ == "__main__":
+    check_params()
     version = get_new_version()
-    changelog = get_changelog(version)
-    create_tag(version)
-    close_milestone(version)
-    create_release(version, changelog)
+    beta_version = "b" in version
+    create_tag(version, beta_version)
+    if not beta_version:
+        changelog = get_changelog(version)
+        close_milestone(version)
+        create_release(version, changelog)
